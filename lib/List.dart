0d3b7888b9889717a3f8fa1a771310c268ce1d2c
heychaewong <163082156+heychaewong@users.noreply.github.com>
2024-05-19 17:56:32 +0900

    Create List.dart

---
+++ lib/List.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'RoomList.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+
+  // 정적 메서드로 addRoomToList 정의
+  static void addRoomToList(String name, String description) {
+    _ListPageState()._addRoom(name, description);
+  }
+}
+
+class _ListPageState extends State<ListPage> {
+  // 변경된 부분: 새로운 방 목록을 저장하는 리스트
+  static List<Map<String, String>> _newRooms = [];
+
+  List<Map<String, String>> _rooms = [
+    {
+      'name': '금공강 4',
+      'description': '라랄라',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Room List'),
+      ),
+      body: RoomList(
+        // 변경된 부분: _rooms와 _newRooms 리스트를 합침
+        rooms: [..._rooms, ..._newRooms],
+        checkedNames: [],
+        onToggleCheck: (name) {
+          // Toggle check logic here
+        },
+      ),
+    );
+  }
+
+  // _addRoom 메서드 정의
+  void _addRoom(String name, String description) {
+    setState(() {
+      // 변경된 부분: 새로운 방 정보는 _newRooms 리스트에 추가
+      _newRooms.add({'name': name, 'description': description});
+    });
+  }
+}
